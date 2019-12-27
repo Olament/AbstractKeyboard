f29ae6e11e4e67a4b593136a2a10186a0a78472e
Zixuan Guo <i@zxguo.me>
2019-12-27 17:55:04 +0800

    Initial Commit

---
+++ AbstractKeyboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AbstractKeyboard
+//
+//  Created by Zixuan on 12/27/19.
+//  Copyright © 2019 Zixuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
